4a00ecc5f7fc8c8975b0772c86fb50b2606654c4
Faraz Mughal <mughalfrazk@gmail.com>
2025-02-25 01:20:25 +0000

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+import tseslint from 'typescript-eslint'
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      // js
+      js.configs.recommended,
+      // ts
+      ...tseslint.configs.recommended,
+      // react
+      react.configs.flat.recommended,
+      // import
+      importPlugin.flatConfigs.recommended,
+      // a11y (accessibility
+      jsxA11y.flatConfigs.recommended,
+      // prettier
+      prettier
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+    settings: {
+      // for eslint-plugin-react to auto detect react version
+      react: {
+        version: 'detect'
+      },
+      // for eslint-plugin-import to use import alias
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json'
+        }
+      }
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'no-console': 'warn',
+      'react/button-has-type': 'error',
+      'react/react-in-jsx-scope': ['off'],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
+  }
+)
